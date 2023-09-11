436f98a253a45d27e9c3e807db0c076e2e578cac
abobaker <aboubkermohammed@gmail.com>
2023-09-10 17:12:46 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include<dtdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all alphabet letters except q and e
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'e')
+			ch++;
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
