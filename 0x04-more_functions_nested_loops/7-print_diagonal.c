4b48aae19893cf8d284b7268be5c6b0666a60d7f
abobaker <aboubkermohammed@gmail.com>
2023-09-15 15:47:42 -0700

    Tasks from 6-10

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -20,7 +20,7 @@ void print_diagonal(int n)
 		{
 			for (space = 1; space <= post; space++)
 				_putchar(' ');
-			_putchar(92); /*equal '/'*/
+			_putchar(92); /*equal '\'*/
 			_putchar('\n');
 		}
 	}
