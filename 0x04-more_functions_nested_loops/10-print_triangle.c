92bda7d001b779e19a0b9f5a74ebdf7065c59d18
abobaker <aboubkermohammed@gmail.com>
2023-09-15 15:43:12 -0700

    Tasks from 6-10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include <main.h>
+
+/**
+ * print_triangle - prints a triangle
+ *
+ * @size: size of the triangle
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_triangle(int size)
+{
+	int hi, ba;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (hi = 1; hi <= size; hi++)
+		{
+			for (ba = 1; ba <= size; ba++)
+			{
+				if ((hi + ba) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print a diagonal line
+ *
+ * @n: is the number of time of times the \
+ *	should be printed
+*/
+
+void print_diagonal(int n)
+{
+
+	int space, post;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (post = 1; post <= n; post++)
+		{
+			for (space = 1; space <= post; space++)
+				_putchar(' ');
+			_putchar(92); /*equal '/'*/
+			_putchar('\n');
+		}
+	}
+}
