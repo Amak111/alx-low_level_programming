ad1430f6bd9c38a92378acf6961956a1da62c992
abobaker <aboubkermohammed@gmail.com>
2023-09-25 13:29:37 -0700

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,7 +5,7 @@
  * @s: The string
  * @accept: Bytes
  *
- * Return: unsigned int.
+ * Return: Unsigned int.
 */
 unsigned int _strspn(char *s, char *accept)
 {
@@ -14,7 +14,7 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] != s[j]; j++)
+		for (j = 0; accept[j] != s[i]; j++)
 		{
 			if (accept[j] == '\0')
 				return (i);
