fe24b8914d22de222fce0de172a5bc4a6cce5148
abobaker <aboubkermohammed@gmail.com>
2023-09-28 05:31:38 -0700

    Tasks from 1 to 5

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Srearch a sting for any of bytes.
+ * @x: base
+ * @y: exposant
+ * Return: Pointer to the byte in 's' that match one of bytes in 'accept'
+ * or NULL if no such byte is found.
+*/
+
+int _pow_recursion(int x, int y)
+{
+
+	if (y < 0)
+		return (-1);
+	else if (y == 1)
+		return (x);
+	else if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+}
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * square - find square root
+ * _sqrt_recursion - find natural square root
+ * @n: int
+ * @v: square root
+ * Return: int
+*/
+
+int square(int n, int v)
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}
+
+
+int square(int n, int v)
+{
+	if (v * v == n)
+		return (v);
+	else if (v * v < n)
+		return (square(n, v + 1));
+	else
+		return (-1);
+}
