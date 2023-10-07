c19e5d921a27699bdb514cacc5aea7f02ecf2db7
abobaker <aboubkermohammed@gmail.com>
2023-10-07 09:10:18 -0700

    Tasks 0 and 1

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * *_memset - fills memory with a constant byte.
+ * @s: Pointer to put the constant
+ * @b: Constant
+ * @n: Max bytes to use
+ * Return: s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n--)
+		*s++ = b;
+
+	return (ptr);
+}
+
+/**
+ * *_calloc - allocates memory for an array, using malloc.
+ * @nmemb: Array length
+ * @size: Size of elements
+ * Return: Pointer
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *f;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	f = malloc(sizeof(int) * nmemb);
+
+	if (f == 0)
+		return (NULL);
+
+	_memset(f, 0, sizeof(int) * nmemb);
+
+	return (f);
+}
