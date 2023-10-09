b046240184890195b602f13bfad531bdda59f618
abobaker <aboubkermohammed@gmail.com>
2023-10-08 17:17:31 -0700

    Fixing task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,42 +2,32 @@
 #include <stdlib.h>
 
 /**
- * *_memset - fills memory with a constant byte.
- * @s: Pointer to put the constant
- * @b: Constant
- * @n: Max bytes to use
- * Return: s
-*/
-
-char *_memset(char *s, char b, unsigned int n)
-{
-	char *ptr = s;
-
-	while (n--)
-		*s++ = b;
-
-	return (ptr);
-}
-
-/**
  * *_calloc - allocates memory for an array, using malloc.
  * @nmemb: Array length
  * @size: Size of each element
+ *
  * Return: Pointer
 */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	int j = 0, l = 0;
 	char *a;
 
 	if (size == 0 || nmemb == 0)
 		return (NULL);
-	a = malloc(sizeof(int) * nmemb);
+
+	l = nmemb * size;
+	a = malloc(l);
 
 	if (a == NULL)
 		return (NULL);
 
-	_memset(a, 0, sizeof(int) * nmemb);
+	while (j < l)
+	{
+		a[j] = 0;
+		j++;
+	}
 
 	return (a);
 }
